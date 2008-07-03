e1b43844d0bb3d31c84c8521d5f73cc6ba3b5407
svg <svg@ff10d734-6443-0410-b6ef-c23fcb60535c>
2008-07-03 14:58:35 +0000

    base framework for make.sh is done
    git-svn-id: https://support.ginsys.be/svn/lt/trunk@51 ff10d734-6443-0410-b6ef-c23fcb60535c

--- make.sh
+++ make.sh
@@ -1,30 +1,42 @@
 #!/bin/sh
 
+command=$1
+book=$2
+
 . config/settings.sh
 . config/functions.sh
 
+books=$( ls books | grep .cfg$ | sed s/.cfg// )
 
 help() {
 	echo
+	echo "linux-training book build script"
+	echo "http://linux-training.be"
+	echo
+	echo $0 "check\t\tcheck some settings"
 	echo $0 "clean\t\tdelete output dir"
 	echo $0 "install\tdownload static build environment to ../static"
 	echo $0 "build\t\tbuild book"
 	echo
-	echo Available books: $( ls books/*.cfg | sed s/.cfg// )
+	echo Available books: $books
 	echo
 	}
 
 check() {
-	set_ROOTDIR || ( echo "It does not look like I'm in the project root dir?"; exit)
 	if [ -z $REVISION ] ; then get_revision ; fi
-	echo "Current dir $(pwd) appears to match the project root dir"
-	echo
 	echo "Subversion repository:\t$SVN_PROJECTDIR"
 	echo "Current version:\t$MAJOR.$MINOR.$REVISION"
-	help
 	}
 
-case "$1" in
+build_book() {
+	echo "[building $book book to implement]"
+	}
+
+##############
+
+set_ROOTDIR || ( echo "It does not look like I'm in the project root dir?"; exit)
+
+case "$command" in
   check)
 	check
 	;;
@@ -34,6 +46,28 @@ case "$1" in
 
   install)
 	README/install_environment.cmd 
+	;;
+  build)
+	check
+	if [ ! -z $book ]
+		then 	# check if $book parameter is one of the available books
+			check=1
+			for entry in $books ; do
+				if [ $entry = $book ]
+					then check=1
+				fi
+			done
+			if [ $check = 1 ]
+				then true
+				else echo "$book is not an available book"; exit
+			fi
+		else
+			$book="default"
+		fi
+
+	echo "Building '$book' book. Hit return to go or ctrl-c to cancel."
+	read me
+	build_book $book
 	;;
 
   *)
