bf3fbbd2ffda789a821d13109c111e252b2efbf4
paul <paul@ff10d734-6443-0410-b6ef-c23fcb60535c>
2008-07-11 23:08:56 +0000

    exported JAVA_HOME increased version number because this version goes online corrected a typo in mod_intro_users corrected three typos in keys.xml tested linuxfun/linuxadm for the first time
    What about changing the toc_depth for linuxfun_toc/linuxadm_toc ?
     --> Will do this manually now without commit...
    
    
    git-svn-id: https://support.ginsys.be/svn/lt/trunk@69 ff10d734-6443-0410-b6ef-c23fcb60535c

--- config/functions.sh
+++ config/functions.sh
@@ -7,7 +7,7 @@
 SVN_PROJECTDIR="https://support.ginsys.be/svn/lt"
 
 # use the correct java runtime for fop on Ubuntu
-JAVA_HOME=/usr/lib/jvm/java-6-sun-1.6.0.06/jre
+export JAVA_HOME=/usr/lib/jvm/java-6-sun-1.6.0.06/jre
 
 # Set the name of the XSL stylesheet
 XSLFILE="lib/lt.xsl"
@@ -16,7 +16,7 @@ XSLFILE="lib/lt.xsl"
 # leave REVISION empty to automagically retrieve it 
 # from local svn working copy or from trunk
 MAJOR=0
-MINOR=963
+MINOR=964
 REVISION=
 
 DATECODE=$(date +%y%m%d | sed s/^0//)
